0f4f64661b098325fdc59e5522e452fc77d6b1b1
Andrew Liu <an6reww@gmail.com>
2024-01-05 13:08:58 -0500

    allow client and server to communicate with one another

--- server.c
+++ server.c
@@ -15,7 +15,9 @@ void subserver_logic(int client_socket){
   char input[BUFFER_SIZE];
   read(client_socket, input, sizeof(input)); //read from client
   printf("Question Recieved (from client): %s \n", input);
-  rot13(input);
+  
+  printf("Answer with yes/no \n");
+  fgets(input, sizeof(input), stdin);
 
 
 //send(client_socket, input, sizeof(input),0);
@@ -25,6 +27,10 @@ printf("Message sent (to client): %s\n", input);
 }
 
 int main(int argc, char *argv[] ) {
+  char userInput[100];
+  printf("Set your word: ");
+  fgets(userInput, sizeof(userInput), stdin);
+
   int listen_socket = server_setup();
   //int num = 0;
   int x = 0;
