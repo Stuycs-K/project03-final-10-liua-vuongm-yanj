90f542a5e55cda02ded2349a069a6e17d4a3a276
Andrew Liu <an6reww@gmail.com>
2024-01-08 15:33:46 -0500

    adding main to semaphore

--- semaphore.c
+++ semaphore.c
@@ -1,3 +1,13 @@
+#include "semaphore.h"
+
+// int main(){
+//     int semd;
+//     semd = semget(694, 1, IPC_CREAT | IPC_EXCL | 0644);
+//     union semun us;
+//     us.val = 1;
+//     int r = semctl(semd, 0, SETVAL, us);
+// }
+
 
 void accessSemaphore(int semid){
     struct sembuf sb; 
@@ -9,7 +19,6 @@ void accessSemaphore(int semid){
 
 void releaseSemaphore(int semid){
     // setting up semaphore ID and struct
-    int semid = semget(694, 1 , 0);
     struct sembuf sb;
     // release semaphore 
     sb.sem_op = 1;
--- semaphore.h
+++ semaphore.h
@@ -14,6 +14,6 @@
 #include <signal.h>
 
 
-
+int semSetup();
 void accessSemaphore(int semid);
 void releaseSemaphore(int semid);
