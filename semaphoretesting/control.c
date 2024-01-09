df6eef5e43dc724ea66bea83a4ec48e93b0b7737
Andrew Liu <an6reww@gmail.com>
2024-01-08 21:58:57 -0500

    testing semaphore usage in semaphore testing file

---
+++ semaphoretesting/control.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <dirent.h>
+#include <signal.h>
+
+union semun {
+  int val;                  //used for SETVAL
+  struct semid_ds *buf;     //used for IPC_STAT and IPC_SET
+  unsigned short  *array;   //used for SETALL
+  struct seminfo  *__buf;
+};
+            
+
+
+void printFile (char filename[]){
+    FILE *file = fopen("story.txt", "r");
+    char buffer [100];
+
+    while(fgets(buffer, sizeof(buffer), file) != NULL){
+        printf("%s", buffer);
+    }
+    fclose(file);
+}
+
+int main(int argc, char *argv[]){
+
+    int semd; // semaphore
+
+    if (argv[1] == NULL)
+    {
+        printf("Input either create, remove, or view\n");
+    }
+
+    // Should make the shared memory, semaphore and file (open the file with the truncate flag).
+    // Set any values that are needed.
+    else if (strcmp("create", argv[1]) == 0)
+    {
+        printf("create ran\n");
+        // semaphore
+        semd = semget(695, 1, IPC_CREAT | IPC_EXCL | 0644);
+        union semun us;
+        us.val = 1;
+        int r = semctl(semd, 0, SETVAL, us);
+    }
+
+    // Remove the shared memory and the semaphore
+    // Display the full contents of the story.
+    else if (strcmp("remove", argv[1]) == 0){
+        printf("remove ran");
+        semctl(semd, 0 , IPC_RMID, 0);
+        printFile("story.txt");
+    }
+
+    // Display the full contents of the story.
+    else if (strcmp("view", argv[1]) == 0){
+        printFile("story.txt");
+    }
+}
---
+++ semaphoretesting/write.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h> 
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <dirent.h>
+#include <signal.h>
+
+union semun {
+  int val;                  //used for SETVAL
+  struct semid_ds *buf;     //used for IPC_STAT and IPC_SET
+  unsigned short  *array;   //used for SETALL
+  struct seminfo  *__buf;
+};
+int err(){
+    printf("errno %d\n",errno);
+    printf("%s\n",strerror(errno));
+    exit(1);
+}
+
+int main(){
+    printf("%s\n" , "Write");
+
+    // Display a message indicating that it attempting to open the resource. This means there will be some message prior to the program blocking.
+    printf("Attempting to access semaphore... \n");
+    
+    // Will attempt to access the resources using the semaphore.
+    // accessing and decreasing by one
+    int semid = semget(695, 1 , 0);
+    struct sembuf sb; 
+    sb.sem_num = 0;
+    sb.sem_flg = SEM_UNDO;
+    sb.sem_op = -1;
+    semop(semid, &sb , 1);
+
+
+    printf("Add another line to the story : ");
+    char userInput[100];
+    // Read the user input from stdin
+    fgets(userInput, sizeof(userInput), stdin);
+
+    // increasing by one and returning
+    // After completing these steps you release the semaphore, and end the write program.
+    sb.sem_op = 1;
+    semop(semid, &sb, 1);
+}
