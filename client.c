ee5e1584fee76728ef338244854748f7ebcf764d
carminvuong <carminvuong@gmail.com>
2024-01-09 15:33:03 -0500

    add code to jiayus code

--- client.c
+++ client.c
@@ -15,13 +15,33 @@ void display(struct player *player){
   printf("%s,%d\n" , player -> name, player -> score);
 }
 
-void clientLogic(int server_socket, struct player* current){
+void clientLogic(int server_socket, struct player* current, int num){
   char userInput[100];
+  char response[100];
+  int t_file;
+
+  int pid = getpid();
+  printf("client pid: %d\n", pid); // TESTING 
+
+  char b[BUFFER_SIZE];
+  sprintf(b, "transcript_%d.txt", pid);
+
+  t_file = open(b, O_RDWR | O_APPEND | O_CREAT, 0611);
+  if (t_file == -1) perror("opening file error");
+
   int temp =0;
   printf("Ask a Question: ");
   // fflush(stdin);
   // fflush(stdout);
   fgets(userInput, sizeof(userInput), stdin);
+
+  char* question = userInput;
+  char buff[100];
+  sprintf(buff, "QUESTION %d: ", num);
+  char* p = buff;
+  write(t_file, p, strlen(p));
+  write(t_file, question, strlen(question)); // put into file?
+
   write(server_socket,userInput,sizeof(userInput));
   //send(server_socket,userInput, sizeof(userInput),0);
 
@@ -32,6 +52,13 @@ void clientLogic(int server_socket, struct player* current){
 
   //printf("%s\n",userInput);
   printf("Answer Received (from server): %s\n", userInput);
+
+  char* r = response;
+  write(t_file, "ANSWER: ", strlen("ANSWER: "));
+  write(t_file, r, strlen(r)); // put into file?
+  write(t_file, "\n", strlen("\n")); // formatting
+
+
   if(strcasecmp(userInput, "yes") == 0){
       current -> score ++;
     //printf("hello");
@@ -53,9 +80,11 @@ int main(int argc, char *argv[] ) {
   fgets(name, sizeof(name), stdin);
   struct player* c = newStruct(name,score);
   //display(c);
+  int clientNum = 1;
   while(1){
     int server_socket = client_tcp_handshake(IP);
     //printf("client connected.\n");
-    clientLogic(server_socket, c);
+    clientLogic(server_socket, c, clientNum);
+    clientNum++;
   }
 }
--- client.h
+++ client.h
@@ -4,7 +4,7 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 struct player* newStruct(char name[35], int score);
-void clientLogic(int server_socket, struct player* current);
+void clientLogic(int server_socket, struct player* current, int num);
 struct player { char name[35]; int score;};
 void display(struct player *player);
 #endif
